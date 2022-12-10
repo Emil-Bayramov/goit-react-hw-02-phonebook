7fcf61350da792dc702685f431fd48998499695f
Emil-Bayramov <105307406+Emil-Bayramov@users.noreply.github.com>
2022-12-10 09:46:45 +0200

    20221210, 0946

---
+++ src/components/App.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Header = styled.h1`
+  width: 200px;
+  margin: 0 auto 30px auto;
+  text-align: center;
+`;
+
+export const SecondaryHeader = styled.h2`
+  width: 200px;
+  margin: 0 auto 30px auto;
+  text-align: left;
+`;
